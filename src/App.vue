bcd6cda06c9fb514e3c9aa790465ccc2794d7f4c
nadir inab <nadir.inab.dev@gmail.com>
2023-03-28 10:35:33 +0000

    axios is installed and data is well fetched and displayed in the console

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+     <Flight />
+  </header>
 </template>
+
+<script >
+  import Flight from "./components/Flight.vue" ;
+  export default {
+    data(){
+      return {
+        test : "tes"
+      }
+    }, 
+    components : {
+      Flight
+    }
+  }
+</script>
 
 <style scoped>
 .logo {
---
+++ src/components/Flight.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <h1>Flights</h1>
+    <div>
+      <ul :key="datum.id" :v-for="data in datum">
+        <li>
+          {{datum.date}},
+          <b>{{datum.from}}</b> To
+          <i>{{datum.to}}</i>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+import axios from "axios";
+import { toRaw, isProxy } from "vue";
+export default {
+  data() {
+    return {
+      name: "airline",
+      data: []
+    };
+  },
+  methods: {
+    handleChange() {
+      this.name = "Nadir api";
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get("http://localhost:3000/flights");
+      this.data = res.data;
+      console.log(toRaw(this.data));
+    } catch (e) {
+      console.log("err " + e);
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style>
+</style>
