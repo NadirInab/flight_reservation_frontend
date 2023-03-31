18b5c043ab16892dabd771f57978b8796e21f6d9
nadir inab <nadir.inab.dev@gmail.com>
2023-03-31 15:09:24 +0000

    components are well imported and css is well added

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,58 @@
 <template>
   <header>
-     <Flight />
+    <Navbar />
+    <Reservations />
+    <div class="container">
+        <!-- <h3>Popular Destinations :</h3> -->
+      <Card  v-for="index in 5" :key="index" title="Rabat" :price="index" imageUrl="../assets/images/rabat.jpg" />
+    </div>
   </header>
 </template>
 
 <script >
-  import Flight from "./components/Flight.vue" ;
-  export default {
-    data(){
-      return {
-        test : "tes"
-      }
-    }, 
-    components : {
-      Flight
-    }
+import Reservations from "./components/Reservations.vue";
+import Navbar from "./components/Navbar.vue";
+import Card from "./components/Card.vue";
+// import Hero from "./components/Hero.vue" ;
+export default {
+  data() {
+    return {
+      test: "tes", 
+      cities : [1, 2, 3, ]
+    };
+  },
+  components: {
+    Navbar,
+    Card,
+    // Loader,
+    // Flight,
+    // Hero,
+    Reservations
   }
+};
 </script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style >
+
+.container {
+  border: 2px solid red;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 1200px;
+  margin: 0 auto;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.card {
+  width: 20rem;
+  height: 400px;
+  margin: 20px;
+  border-radius: 10px;
+  background-color: #f2f2f2;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease-in-out;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.card:hover {
+  transform: scale(1.05);
 }
 </style>
