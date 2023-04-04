c6d64fe896cbf82ba786dd994f74b7054c57f6d3
nadir inab <nadir.inab.dev@gmail.com>
2023-04-04 09:14:32 +0000

    vue router is added successfully

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,30 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
+    path: '/About',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/Flights',
+    name: 'Flights',
+    component: () => import('../views/Flights.vue')
+  },
+  {
+    path: '/Sign',
+    name: 'Sign',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/Sign.vue')
+  }, 
+
 ]
 
 const router = createRouter({
